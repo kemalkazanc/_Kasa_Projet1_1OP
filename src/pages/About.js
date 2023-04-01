c9f504408bb9ce5bec177e660dfa9fc1af2e6de4
Kemal Kazanc <keemalkazanc@gmail.com>
2023-04-01 13:33:44 +0200

    initialisation du projet

---
+++ src/pages/About.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Navigation from "../../src/styles/components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>À propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, neque
+        sed debitis aliquid assumenda velit aut facere consequatur dolorem
+        laudantium quae pariatur mollitia voluptas hic culpa. Omnis tenetur
+        repellat recusandae officiis id culpa facere quaerat qui, fugit
+        asperiores illo repudiandae aliquam dignissimos quod hic quas dicta.
+        Expedita reiciendis veniam maiores quam hic, labore sed voluptatum modi
+        impedit, dolores amet eius quod facere. Quaerat enim quasi possimus
+        nihil magni facilis, ipsa quia soluta delectus inventore adipisci quas,
+        consequatur officiis dolorem consequuntur. Ipsa, officiis blanditiis
+        sunt vitae suscipit molestias alias perspiciatis quasi exercitationem
+        quaerat vel consectetur quam voluptatum consequatur eos cumque magnam ab
+        placeat reprehenderit sequi labore incidunt amet sed nemo! Quod
+        architecto, omnis reiciendis libero possimus quaerat non expedita magnam
+        vero nihil, facere atque eos consequatur nostrum. Animi mollitia
+        nesciunt velit itaque consectetur quod ipsam reprehenderit modi ipsa
+        distinctio odio in atque assumenda quisquam tenetur minus, dicta culpa,
+        voluptate molestias quam unde. Nemo odit rerum eum, quaerat eius
+        necessitatibus. Laborum corporis sint modi saepe obcaecati, id quas qui
+        ex similique eius aperiam voluptate explicabo, impedit libero magnam
+        fuga sapiente non, optio praesentium? Voluptate quod veniam pariatur
+        quae, necessitatibus incidunt aut nisi ab alias possimus animi, nam
+        itaque corporis assumenda consectetur. Sapiente?
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        facilis hic iusto cumque, natus magnam nemo velit tempore praesentium
+        ipsam voluptas quibusdam? Eaque reiciendis dolore omnis delectus sequi,
+        laudantium corrupti assumenda nobis error iste commodi magni dolores
+        ducimus debitis. Et molestiae possimus earum, aliquam minima voluptas
+        architecto officiis ducimus ea adipisci eveniet minus qui eligendi
+        expedita quae quia, vel repellat neque nobis quam autem delectus
+        tenetur? Possimus consequuntur eos molestias?
+      </p>
+    </div>
+  );
+};
+
+export default About;
